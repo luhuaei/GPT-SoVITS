db756e368aa7aa53c8f6caffd9c38f0bc3bc89d9
luhuaei <luhuaei@gmail.com>
2026-03-10 07:33:03 +0000

    Optimize Jetson image footprint

--- scripts/jetson/build_on_jetson.sh
+++ scripts/jetson/build_on_jetson.sh
@@ -10,7 +10,14 @@ REMOTE_DIR="${JETSON_DIR:-/home/nvidia/GPT-SoVITS}"
 
 ssh -o StrictHostKeyChecking=no "$REMOTE_HOST" \
   "test -f '$REMOTE_DIR/jetson/models/assets/open_jtalk_dic_utf_8-1.11.tar.gz' && \
-   test -d '$REMOTE_DIR/jetson/models/pretrained_models' && \
+   test -f '$REMOTE_DIR/jetson/models/pretrained_models/gsv-v2final-pretrained/s1bert25hz-5kh-longer-epoch=12-step=369668.ckpt' && \
+   test -f '$REMOTE_DIR/jetson/models/pretrained_models/gsv-v2final-pretrained/s2G2333k.pth' && \
+   test -f '$REMOTE_DIR/jetson/models/pretrained_models/fast_langdetect/lid.176.bin' && \
+   test -d '$REMOTE_DIR/jetson/models/pretrained_models/chinese-hubert-base' && \
+   test -d '$REMOTE_DIR/jetson/models/pretrained_models/chinese-roberta-wwm-ext-large' && \
    test -d '$REMOTE_DIR/jetson/models/G2PWModel' && \
-   test -d '$REMOTE_DIR/jetson/models/nltk_data' && \
+   test -d '$REMOTE_DIR/jetson/models/nltk_data/corpora/cmudict' && \
+   test -f '$REMOTE_DIR/jetson/models/nltk_data/corpora/cmudict.zip' && \
+   test -f '$REMOTE_DIR/jetson/models/nltk_data/taggers/averaged_perceptron_tagger.zip' && \
+   test -d '$REMOTE_DIR/jetson/models/nltk_data/taggers/averaged_perceptron_tagger_eng' && \
    cd '$REMOTE_DIR' && DOCKER_BUILDKIT=1 docker build -f Dockerfile.jetson -t gpt-sovits-jetson:local ."
--- scripts/jetson/prepare_models_on_jetson.sh
+++ scripts/jetson/prepare_models_on_jetson.sh
@@ -36,32 +36,109 @@ esac
 
 "$SCRIPT_DIR/sync_to_jetson.sh"
 
-ssh -o StrictHostKeyChecking=no "$REMOTE_HOST" "\
-  set -euo pipefail; \
-  mkdir -p '$REMOTE_DIR/jetson/models/assets' '$REMOTE_DIR/jetson/models/pretrained_models' '$REMOTE_DIR/jetson/models/G2PWModel' '$REMOTE_DIR/jetson/models/nltk_data' '$REMOTE_DIR/jetson/cache'; \
-  cd '$REMOTE_DIR/jetson/cache'; \
-  wget -c -O pretrained_models.zip '$PRETRAINED_URL'; \
-  wget -c -O G2PWModel.zip '$G2PW_URL'; \
-  wget -c -O nltk_data.zip '$NLTK_URL'; \
-  wget -c -O averaged_perceptron_tagger.zip '$NLTK_TAGGER_URL'; \
-  wget -c -O averaged_perceptron_tagger_eng.zip '$NLTK_TAGGER_ENG_URL'; \
-  wget -c -O open_jtalk_dic_utf_8-1.11.tar.gz '$OPENJTALK_URL'; \
-  rm -rf '$REMOTE_DIR/jetson/models/pretrained_models'/*; \
-  rm -rf '$REMOTE_DIR/jetson/models/G2PWModel'/*; \
-  rm -rf '$REMOTE_DIR/jetson/models/nltk_data'/*; \
-  unzip -oq pretrained_models.zip -d '$REMOTE_DIR/jetson/cache/pretrained_unpack'; \
-  if [ -d '$REMOTE_DIR/jetson/cache/pretrained_unpack/pretrained_models' ]; then cp -a '$REMOTE_DIR/jetson/cache/pretrained_unpack/pretrained_models/.' '$REMOTE_DIR/jetson/models/pretrained_models/'; else cp -a '$REMOTE_DIR/jetson/cache/pretrained_unpack/.' '$REMOTE_DIR/jetson/models/pretrained_models/'; fi; \
-  rm -rf '$REMOTE_DIR/jetson/cache/pretrained_unpack'; \
-  unzip -oq G2PWModel.zip -d '$REMOTE_DIR/jetson/cache/g2pw_unpack'; \
-  if [ -d '$REMOTE_DIR/jetson/cache/g2pw_unpack/G2PWModel' ]; then cp -a '$REMOTE_DIR/jetson/cache/g2pw_unpack/G2PWModel/.' '$REMOTE_DIR/jetson/models/G2PWModel/'; else cp -a '$REMOTE_DIR/jetson/cache/g2pw_unpack/.' '$REMOTE_DIR/jetson/models/G2PWModel/'; fi; \
-  rm -rf '$REMOTE_DIR/jetson/cache/g2pw_unpack'; \
-  unzip -oq nltk_data.zip -d '$REMOTE_DIR/jetson/cache/nltk_unpack'; \
-  if [ -d '$REMOTE_DIR/jetson/cache/nltk_unpack/nltk_data' ]; then cp -a '$REMOTE_DIR/jetson/cache/nltk_unpack/nltk_data/.' '$REMOTE_DIR/jetson/models/nltk_data/'; else cp -a '$REMOTE_DIR/jetson/cache/nltk_unpack/.' '$REMOTE_DIR/jetson/models/nltk_data/'; fi; \
-  rm -rf '$REMOTE_DIR/jetson/cache/nltk_unpack'; \
-  mkdir -p '$REMOTE_DIR/jetson/models/nltk_data/taggers'; \
-  cp -f averaged_perceptron_tagger.zip '$REMOTE_DIR/jetson/models/nltk_data/taggers/averaged_perceptron_tagger.zip'; \
-  cp -f averaged_perceptron_tagger_eng.zip '$REMOTE_DIR/jetson/models/nltk_data/taggers/averaged_perceptron_tagger_eng.zip'; \
-  unzip -oq averaged_perceptron_tagger.zip -d '$REMOTE_DIR/jetson/models/nltk_data/taggers'; \
-  unzip -oq averaged_perceptron_tagger_eng.zip -d '$REMOTE_DIR/jetson/models/nltk_data/taggers'; \
-  cp -f open_jtalk_dic_utf_8-1.11.tar.gz '$REMOTE_DIR/jetson/models/assets/open_jtalk_dic_utf_8-1.11.tar.gz'; \
-  find '$REMOTE_DIR/jetson/models' -maxdepth 2 -type f | sed -n '1,80p'"
+ssh -o StrictHostKeyChecking=no "$REMOTE_HOST" bash -s -- \
+  "$REMOTE_DIR" \
+  "$PRETRAINED_URL" \
+  "$G2PW_URL" \
+  "$NLTK_URL" \
+  "$NLTK_TAGGER_URL" \
+  "$NLTK_TAGGER_ENG_URL" \
+  "$OPENJTALK_URL" <<'REMOTE'
+set -euo pipefail
+
+REMOTE_DIR="$1"
+PRETRAINED_URL="$2"
+G2PW_URL="$3"
+NLTK_URL="$4"
+NLTK_TAGGER_URL="$5"
+NLTK_TAGGER_ENG_URL="$6"
+OPENJTALK_URL="$7"
+
+MODEL_ROOT="$REMOTE_DIR/jetson/models"
+CACHE_ROOT="$REMOTE_DIR/jetson/cache"
+PRETRAINED_ROOT="$MODEL_ROOT/pretrained_models"
+PRETRAINED_CACHE="$CACHE_ROOT/pretrained_unpack"
+G2PW_CACHE="$CACHE_ROOT/g2pw_unpack"
+NLTK_CACHE="$CACHE_ROOT/nltk_unpack"
+
+mkdir -p "$MODEL_ROOT/assets" "$PRETRAINED_ROOT" "$MODEL_ROOT/G2PWModel" "$MODEL_ROOT/nltk_data" "$CACHE_ROOT"
+cd "$CACHE_ROOT"
+
+download_if_missing() {
+  local target="$1"
+  local url="$2"
+  if [ -s "$target" ]; then
+    echo "Reusing cached artifact: $target"
+    return
+  fi
+  wget -O "$target" "$url"
+}
+
+download_if_missing pretrained_models.zip "$PRETRAINED_URL"
+download_if_missing G2PWModel.zip "$G2PW_URL"
+download_if_missing nltk_data.zip "$NLTK_URL"
+download_if_missing averaged_perceptron_tagger.zip "$NLTK_TAGGER_URL"
+download_if_missing averaged_perceptron_tagger_eng.zip "$NLTK_TAGGER_ENG_URL"
+download_if_missing open_jtalk_dic_utf_8-1.11.tar.gz "$OPENJTALK_URL"
+
+rm -rf "$PRETRAINED_ROOT"/* "$MODEL_ROOT/G2PWModel"/* "$MODEL_ROOT/nltk_data"/*
+rm -rf "$PRETRAINED_CACHE" "$G2PW_CACHE" "$NLTK_CACHE"
+mkdir -p "$PRETRAINED_CACHE" "$G2PW_CACHE" "$NLTK_CACHE"
+
+unzip -oq pretrained_models.zip -d "$PRETRAINED_CACHE"
+PRETRAINED_SRC="$PRETRAINED_CACHE"
+if [ -d "$PRETRAINED_CACHE/pretrained_models" ]; then
+  PRETRAINED_SRC="$PRETRAINED_CACHE/pretrained_models"
+fi
+
+mkdir -p \
+  "$PRETRAINED_ROOT/gsv-v2final-pretrained" \
+  "$PRETRAINED_ROOT/chinese-hubert-base" \
+  "$PRETRAINED_ROOT/chinese-roberta-wwm-ext-large" \
+  "$PRETRAINED_ROOT/fast_langdetect"
+
+cp -a "$PRETRAINED_SRC/chinese-hubert-base/." "$PRETRAINED_ROOT/chinese-hubert-base/"
+cp -a "$PRETRAINED_SRC/chinese-roberta-wwm-ext-large/." "$PRETRAINED_ROOT/chinese-roberta-wwm-ext-large/"
+cp -a "$PRETRAINED_SRC/fast_langdetect/." "$PRETRAINED_ROOT/fast_langdetect/"
+cp -f "$PRETRAINED_SRC/gsv-v2final-pretrained/s1bert25hz-5kh-longer-epoch=12-step=369668.ckpt" \
+  "$PRETRAINED_ROOT/gsv-v2final-pretrained/"
+cp -f "$PRETRAINED_SRC/gsv-v2final-pretrained/s2G2333k.pth" \
+  "$PRETRAINED_ROOT/gsv-v2final-pretrained/"
+rm -rf "$PRETRAINED_CACHE"
+
+unzip -oq G2PWModel.zip -d "$G2PW_CACHE"
+if [ -d "$G2PW_CACHE/G2PWModel" ]; then
+  cp -a "$G2PW_CACHE/G2PWModel/." "$MODEL_ROOT/G2PWModel/"
+else
+  cp -a "$G2PW_CACHE/." "$MODEL_ROOT/G2PWModel/"
+fi
+rm -rf "$G2PW_CACHE"
+
+unzip -oq nltk_data.zip -d "$NLTK_CACHE"
+NLTK_SRC="$NLTK_CACHE"
+if [ -d "$NLTK_CACHE/nltk_data" ]; then
+  NLTK_SRC="$NLTK_CACHE/nltk_data"
+fi
+
+mkdir -p \
+  "$MODEL_ROOT/nltk_data/corpora/cmudict" \
+  "$MODEL_ROOT/nltk_data/taggers/averaged_perceptron_tagger" \
+  "$MODEL_ROOT/nltk_data/taggers/averaged_perceptron_tagger_eng"
+
+if [ -d "$NLTK_SRC/corpora/cmudict" ]; then
+  cp -a "$NLTK_SRC/corpora/cmudict/." "$MODEL_ROOT/nltk_data/corpora/cmudict/"
+fi
+if [ -f "$NLTK_SRC/corpora/cmudict.zip" ]; then
+  cp -f "$NLTK_SRC/corpora/cmudict.zip" "$MODEL_ROOT/nltk_data/corpora/cmudict.zip"
+fi
+rm -rf "$NLTK_CACHE"
+
+unzip -oq averaged_perceptron_tagger.zip -d "$MODEL_ROOT/nltk_data/taggers"
+unzip -oq averaged_perceptron_tagger_eng.zip -d "$MODEL_ROOT/nltk_data/taggers"
+cp -f averaged_perceptron_tagger.zip "$MODEL_ROOT/nltk_data/taggers/averaged_perceptron_tagger.zip"
+
+cp -f open_jtalk_dic_utf_8-1.11.tar.gz "$MODEL_ROOT/assets/open_jtalk_dic_utf_8-1.11.tar.gz"
+
+du -sh "$PRETRAINED_ROOT" "$MODEL_ROOT/G2PWModel" "$MODEL_ROOT/nltk_data"
+find "$MODEL_ROOT" -maxdepth 3 -type f | sort | sed -n '1,120p'
+REMOTE
